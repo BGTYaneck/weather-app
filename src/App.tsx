5d58a45dd5e2118c6dabf3d4551fdf37a107c04a
BGTYaneck <bezulak.jr@o2.pl>
2023-05-22 20:13:36 +0200

    Changed into MUI Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import React from "react";
-import cities from "cities.json";
-import { Autocomplete } from "@mantine/core";
-import { IconWorldDown } from "@tabler/icons-react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center flex-col p-3 pt-5 gap-5">
-      <div className="w-10/12 min-h-[9rem] rounded-3xl bg-[#1c1b22] flex justify-center items-center">
-        <p className="opacity-50">Your Favourited Cities will appear here!</p>
-      </div>
-      <div className="flex flex-row flex-wrap w-10/12 h-[32rem] gap-3 ">
-        <div className="w-[26rem] bg-[#1c1b22] rounded-3xl h-full flex justify-center ">
-          <Autocomplete
-            className="w-[23rem] mt-5 rounded-2xl"
-            hoverOnSearchChange
-            limit={12}
-            icon={<IconWorldDown />}
-            placeholder="Select a city/region..."
-            data={[]}
-          />
-        </div>
-        <div className=" w-[52rem] bg-[#1c1b22] rounded-3xl h-full flex justify-center items-center">
-          <p className="opacity-50">...</p>
-        </div>
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }} className="p-8">
+      <Grid container spacing={2}>
+        <Grid item xl={12} md={12} xs={12}>
+          <Paper className="min-h-[9rem] flex justify-center items-center">
+            Your favourited cities will appear here!
+          </Paper>
+        </Grid>
+        <Grid item xl={3} md={4} xs={12}>
+          <Paper className="flex h-[34rem] justify-center items-center">
+            Search bar & data
+          </Paper>
+        </Grid>
+        <Grid item xl={9} md={8} xs={12}>
+          <Paper className="flex h-[34rem] justify-center items-center">
+            Graph
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
